52ff7e68785e39e4a9813e347d25cb94098942bc
Abhishek Kumar <ak3037308@gmail.com>
2024-09-05 18:19:34 +0530

    added privacy-policy page

---
+++ src/components/privacy/privacy-policy.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+export const PrivacyPolicy = () => {
+  return(
+<main id="content">
+    {/* <!-- Heading --> */}
+    <div className="md:max-w-screen-sm text-center px-4 sm:px-6 lg:px-8 pt-24 pb-6 mx-auto">
+      <h1 className="mb-2 text-2xl font-bold md:text-4xl text-gray-900">Privacy Policy</h1>
+      <p className="text-gray-900 ">Last updated: January 31, 2024</p>
+    </div>
+    {/* <!-- End Heading --> */}
+
+    {/* <!-- Content --> */}
+    <div className="md:max-w-screen-sm lg:max-w-[992px] px-4 sm:px-6 lg:px-8 pb-12 md:pt-6 sm:pb-20 mx-auto">
+      <div className="grid gap-4 md:gap-8">
+        <div>
+          <p className="mb-8 text-gray-700 text-xl">Welcome to TOSGateway. This Privacy Policy outlines our practices regarding the collection, use, and sharing of personal information through our digital products and services, including TOSGateway (TOS.systems). By using our services, provided by TOSGateway. you agree to the collection and use of information as described in this policy.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Information Collected</h2>
+          <p className="mb-5  text-gray-700">At TOSGateway, your privacy is our priority. We do not share, sell, or rent your personal information to third parties. All data stored via our services is owned by you or your business and can be transferred or deleted upon request, though this may limit access to certain content.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Account Information</h2>
+          <p className="mb-5  text-gray-700">Creating an account at TOSGateway requires providing personal details such as your name, email address, and password, necessary for account setup and access.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Payment Information</h2>
+          <p className="mb-5  text-gray-700">For payment processing, we use Paddle (paddle.com). During transactions, Paddle may ask for personal details like your name, address, email, and credit card information. Paddle's privacy policy, available at Paddle's Privacy Terms, governs their data handling. TOSGateway is not responsible for Paddle's practices.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Usage Information</h2>
+          <p className="mb-5  text-gray-700">We collect data on user interactions with TOSGateway, including usage patterns and device information, to improve our platform.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Legal Compliance and General Use</h2>
+          <p className="mb-5  text-gray-700">TOSGateway may release personal information if required by law, such as in response to a court order or fraud investigation. We also use aggregated, non-identifiable user data for general analytical purposes.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Data Sharing and Disclosure</h2>
+          <p className="mb-5  text-gray-700">We use third-party services like Google Analytics for analytics purposes. These parties access your data only for performing tasks on our behalf and are obligated not to disclose or use it for other purposes.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">International Data Transfer</h2>
+          <p className="mb-5  text-gray-700">Your information may be transferred to and maintained on computers outside your jurisdiction. Our primary servers are located in the US.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Your Rights</h2>
+          <p className="mb-5  text-gray-700">You have the right to access, correct, and request the deletion of your personal data, with certain limitations on access to our products and services.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Data Retention</h2>
+          <p className="mb-5  text-gray-700">We retain personal data as long as your account is active or as needed to provide services.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Security</h2>
+          <p className="mb-5  text-gray-700">We are committed to the security of your data, implementing encryption and regular security standards.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Children's Privacy</h2>
+          <p className="mb-5  text-gray-700">TOSGateway is intended for use by individuals who have reached the legal age in their jurisdiction and reside in regions where such use is permissible. We do not knowingly collect data from individuals under the legal age.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Changes to Privacy Policy</h2>
+          <p className="mb-5  text-gray-700">We may modify this policy at any time. Continued use of TOSGateway signifies acceptance of these changes.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Contact Us</h2>
+          <p className="mb-5  text-gray-700">For questions about this Privacy Policy or our privacy practices, contact us via live chat on our website.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Agreement</h2>
+          <p className="mb-5  text-gray-700">By using TOSGateway products and services, you acknowledge your understanding and agreement to this Privacy Policy.</p>
+
+          <h2 className="text-lg font-semibold mb-2  text-gray-900">Privacy Changes</h2>
+          <p className="mb-5  text-gray-700">If we change our privacy policy we will post those changes on this page. Registered users will be sent an email that outlines changes made to the privacy policy.</p>
+        </div>
+      </div>
+    </div>
+    {/* <!-- End Content --> */}
+  </main>
+  )
+}
